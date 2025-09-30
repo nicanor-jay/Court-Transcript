872a22c4cb51a9dcba91ab5dd4bc9b43a4fc6d12
lenaverse <malena.brignola@yahoo.es>
2025-09-30 11:57:38 +0100

    Added ECR for the ECS task and fixed file structure + consistent naming

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,10 @@ module "phase-one" {
     ACCESS_KEY = var.ACCESS_KEY
     SECRET_ACCESS_KEY = var.SECRET_ACCESS_KEY
     REGION = var.REGION
+    DB_NAME = var.DB_NAME
+    DB_USERNAME = var.DB_USERNAME
+    DB_PASSWORD = var.DB_PASSWORD
+    SUBNET_IDS = var.SUBNET_IDS
 }
 
 module "phase-two" {
@@ -10,4 +14,8 @@ module "phase-two" {
     ACCESS_KEY = var.ACCESS_KEY
     SECRET_ACCESS_KEY = var.SECRET_ACCESS_KEY
     REGION = var.REGION
+    DB_NAME = var.DB_NAME
+    DB_USERNAME = var.DB_USERNAME
+    DB_PASSWORD = var.DB_PASSWORD
+    SUBNET_IDS = var.SUBNET_IDS
 }
--- terraform/phase-one/main.tf
+++ terraform/phase-one/main.tf
@@ -1,4 +1,37 @@
-resource "aws_ecr_repository" "project-ecr" {
+# DB subnet group
+resource "aws_db_subnet_group" "c19_courts_db_subnet_group" {
+  name       = "c19-courts-db-subnet-group"
+  subnet_ids = var.SUBNET_IDS
+
+  tags = {
+    Name = "c19-courts-db-subnet-group"
+  }
+}
+
+# DB instance
+resource "aws_db_instance" "c19_courts_db" {
+  identifier        = "c19-courts-db"
+  engine            = "postgres"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+
+  db_name   = var.DB_NAME
+  username  = var.DB_USERNAME
+  password  = var.DB_PASSWORD
+
+  db_subnet_group_name = aws_db_subnet_group.c19_courts_db_subnet_group.name
+  publicly_accessible  = true
+  skip_final_snapshot  = true
+}
+
+# ECR to host lambda
+resource "aws_ecr_repository" "lambda-ecr" {
   name                 = "c19-courts-daily-pipeline"
+  image_tag_mutability = "MUTABLE"
+}
+
+# ECR to host the ECS Task
+resource "aws_ecr_repository" "ecs-ecr" {
+  name                 = "c19-courts-ecs-task"
   image_tag_mutability = "MUTABLE"
 }
