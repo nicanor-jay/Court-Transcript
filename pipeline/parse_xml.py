b4aae47638d301ffee44bcd5fbc0be52eeeb0c41
Arshin Behjat <trainee.arshin.behjat@sigmalabs.co.uk>
2025-10-02 12:46:00 +0100

    Use .strip() to remove {} instead

--- pipeline/parse_xml.py
+++ pipeline/parse_xml.py
@@ -74,7 +74,10 @@ def get_headings(root: "etree._ElementTree") -> list["etree._Element"]:
     # We need to remove any <toc> (Table of Contents) tags since
     # this will lead to duplicates and may mess with text extraction
     # NAMESPACE[1:-1] is used to remove the {} brackets
-    toc_elements = root.xpath("//n:toc", namespaces={"n": NAMESPACE[1:-1]})
+    toc_elements = root.xpath(
+        "//n:toc",
+        namespaces={"n": NAMESPACE.strip("{}")}
+    )
     for toc in toc_elements:
         toc.getparent().remove(toc)
 
