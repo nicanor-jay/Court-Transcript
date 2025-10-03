0116caba5d783213ca5d80287301641128e5a3f5
Arshin Behjat <trainee.arshin.behjat@sigmalabs.co.uk>
2025-10-03 15:18:05 +0100

    Make case_fetcher import path dynamic

--- xml_extraction/path_bootstrap_util.py
+++ xml_extraction/path_bootstrap_util.py
@@ -1,8 +1,14 @@
 """Very hacky way of accessing case_fetcher.py from here."""
 
 import sys
+from os.path import abspath, isdir
 
 # insert case_fetcher dir into Python path
 # this lets us import case_fetcher.py
-sys.path.append("/Users/arshinbehjat/Documents/sigma-labs/Court-Transcript/case_fetcher")
+case_fetcher_path = abspath("../case_fetcher")
+if not isdir(case_fetcher_path):
+    case_fetcher_path = abspath("case_fetcher")
+
+sys.path.append(case_fetcher_path)
+
 from case_fetcher import fetch_feed, get_xml_entries, load_all_xml
