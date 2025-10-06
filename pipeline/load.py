b783ee656360ba2202d26214af5f35a9576b51ef
riaz <riaz.rahman1751@gmail.com>
2025-10-06 12:46:47 +0100

    Pass db connection as a parameter to each function instead of creating a new one

--- pipeline/load.py
+++ pipeline/load.py
@@ -31,9 +31,8 @@ def get_db_connection() -> connection:
         return None
 
 
-def get_title_id(title_name: str) -> int:
+def get_title_id(conn: connection, title_name: str) -> int:
     """ Returns the title ID that matches the given judge title. """
-    conn = get_db_connection()
 
     with conn.cursor(cursor_factory=RealDictCursor) as cur:
         query = """
@@ -46,10 +45,8 @@ def get_title_id(title_name: str) -> int:
         return cur.fetchone()
 
 
-def check_judge_exists(judges: list) -> list[int]:
+def check_judge_exists(conn: connection, judges: list) -> list[int]:
     """ Returns the judge_id if the judge exists in the judge table. """
-
-    conn = get_db_connection()
 
     judge_ids = []
     for judge in judges:
@@ -68,9 +65,8 @@ def check_judge_exists(judges: list) -> list[int]:
     return [id for id in judge_ids if id is not None]
 
 
-def get_judgement_id(ruling: str) -> int:
+def get_judgement_id(conn: connection, ruling: str) -> int:
     """ Returns the judgement ID that matches the ruling. """
-    conn = get_db_connection()
 
     with conn.cursor(cursor_factory=RealDictCursor) as cur:
         query = """
@@ -84,10 +80,8 @@ def get_judgement_id(ruling: str) -> int:
     return result if result else "Not found"
 
 
-def get_court_id(court_name: str) -> int:
+def get_court_id(conn: connection, court_name: str) -> int:
     """ Returns the court ID for a given court name. """
-
-    conn = get_db_connection()
 
     with conn.cursor(cursor_factory=RealDictCursor) as cur:
         query = """
@@ -100,9 +94,8 @@ def get_court_id(court_name: str) -> int:
         return cur.fetchone()
 
 
-def insert_into_court(court: str) -> int:
+def insert_into_court(conn: connection, court: str) -> int:
     """ Insert a a new row into the court table and return its id. """
-    conn = get_db_connection()
 
     with conn.cursor(cursor_factory=RealDictCursor) as cur:
         query = """
@@ -116,9 +109,8 @@ def insert_into_court(court: str) -> int:
     return cur.fetchone()
 
 
-def insert_into_judge_hearing(judge_ids: list, hearing_id: int) -> None:
+def insert_into_judge_hearing(conn: connection, judge_ids: list, hearing_id: int) -> None:
     """ Inserts records in the judge_hearing table. """
-    conn = get_db_connection()
 
     for judge_id in judge_ids:
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
@@ -130,14 +122,14 @@ def insert_into_judge_hearing(judge_ids: list, hearing_id: int) -> None:
             conn.commit()
 
 
-def insert_into_hearing(hearing: dict, metadata: dict) -> None:
+def insert_into_hearing(conn: connection, hearing: dict, metadata: dict) -> None:
     """ Inserts a new row in the hearing table. """
-    judge_ids = check_judge_exists(metadata.get('judges'))
+    judge_ids = check_judge_exists(conn, metadata.get('judges'))
     if judge_ids:
-        judgement_id = get_judgement_id(hearing.get(('ruling')))
-        court_id = get_court_id(metadata.get('court'))
+        judgement_id = get_judgement_id(conn, hearing.get(('ruling')))
+        court_id = get_court_id(conn, metadata.get('court'))
         if court_id is None:
-            court_id = insert_into_court(metadata.get('court'))
+            court_id = insert_into_court(conn, metadata.get('court'))
         hearing_title = metadata.get('title')
         hearing_date = metadata.get('verdict_date')
         citation = metadata.get('citation')
@@ -145,7 +137,6 @@ def insert_into_hearing(hearing: dict, metadata: dict) -> None:
         anomaly = hearing.get('anomaly')
         hearing_url = metadata.get('url')
 
-        conn = get_db_connection()
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
             query = """
             INSERT INTO hearing
@@ -157,4 +148,4 @@ def insert_into_hearing(hearing: dict, metadata: dict) -> None:
                         hearing_title, hearing_date, description, hearing_url, anomaly))
             hearing_id = cur.fetchone()
             conn.commit()
-        insert_into_judge_hearing(judge_ids, hearing_id)  
+        insert_into_judge_hearing(conn, judge_ids, hearing_id)
