22805f074d0eabc8bd0824525d690894bdcfbaef
Arshin Behjat <trainee.arshin.behjat@sigmalabs.co.uk>
2025-10-07 12:12:07 +0100

    Add seed_db.sql to setup script

--- database/setup_reset.sh
+++ database/setup_reset.sh
@@ -2,3 +2,4 @@ source ../.env
 export PGPASSWORD=$DB_PASSWORD
 
 psql -h $DB_HOST -U $DB_USERNAME -p $DB_PORT -d $DB_NAME -f schema.sql
+psql -h $DB_HOST -U $DB_USERNAME -p $DB_PORT -d $DB_NAME -f seed_db.sql
